a33df35d6091617e794fb5bac07666673cc011ed
RodrigoIbarraSanchez <lookatthemoon@live.com.mx>
2017-01-22 23:22:45 -0600

    registro listo

--- src/web.js
+++ src/web.js
@@ -1,24 +1,38 @@
 // Dependencies
-var express = require('express')
-var morgan = require("morgan")
-var path = require("path")
+var express = require('express');
+var morgan = require("morgan");
+var path = require("path");
+var auth0Valerio = require('valerio-auth0');
 
 // Project dependencies
-var config = require('./config')
-
-var app = express()
+var config = require('./config');
 
 // Settings
-var app = express()
-app.set('port', (config.port))
-app.use(morgan(config.environment))
+var app = express();
+app.set('port', (config.port));
+app.use(morgan(config.environment));
 
 // Public folder
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Auth0 Routes
+app.use('/', auth0Valerio.router({
+    domain: 'devschool.auth0.com',
+    client_id: 'zf0TgECPxfqrQnVFuw57BadyJhjPTQ0k',
+    client_secret: 'zM3iYU7G3sdnOCFDF0igLRvj5o2is7nNxjBZzwp7k8aIsIKlxG2wPESzlpwx7evb',
+    url: process.env.URL || 'http://localhost:5000',
+    session_secret: 'cualquiercosa'
+}));
+
+app.get('/dashboard', auth0Valerio.middlware.requiresLogin, function(req, res) {
+    // res.json(res.locals.user);
+    // res.send(res.locals.user.name);
+    res.send('Pagina de Usuario');
+});
 
 // Error 404
 app.use(function(req, res, next){
-	res.status(404)
+	res.status(404);
 
 	// respond with html page
 	if (req.accepts('html')) {
@@ -32,9 +46,9 @@ app.use(function(req, res, next){
 
 	// default to plain-text. send()
 	return res.type('txt').send('Not found')
-})
+});
 
 // Start Server
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'))
-})
+});
